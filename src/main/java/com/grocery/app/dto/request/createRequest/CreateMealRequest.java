cd487e451b7e10c6c40f24f2417aca6359c0924b
littleamaterasu <clgtthangtung@gmail.com>
2024-11-06 22:58:47 +0700

    init create and update request for meal module

--- src/main/java/com/grocery/app/dto/request/createRequest/CreateMealRequest.java
+++ src/main/java/com/grocery/app/dto/request/createRequest/CreateMealRequest.java
@@ -5,13 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class CreateMealRequest {
     Long userId;
+    Long familyId;
     String name;
     String term;
-
+    String date;
+    ArrayList<Long> recipeList;
 }
--- src/main/java/com/grocery/app/dto/request/updateRequest/UpdateMealRequest.java
+++ src/main/java/com/grocery/app/dto/request/updateRequest/UpdateMealRequest.java
@@ -1,4 +1,22 @@
 package com.grocery.app.dto.request.updateRequest;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UpdateMealRequest {
+    Long mealId;
+    Long userId;
+    Long familyId;
+    String name;
+    String term;
+    String date;
+    ArrayList<Long> recipeList;
 }
