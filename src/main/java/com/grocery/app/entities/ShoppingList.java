af4a7682561c3ab7f344fb8f3ca6ba6e5fb391a3
littleamaterasu <clgtthangtung@gmail.com>
2024-11-06 18:18:42 +0700

    add foreign key to shopping list

--- src/main/java/com/grocery/app/entities/ShoppingList.java
+++ src/main/java/com/grocery/app/entities/ShoppingList.java
@@ -1,8 +1,7 @@
 package com.grocery.app.entities;
 
 import com.grocery.app.config.StatusConfig;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,21 +9,35 @@ import lombok.NoArgsConstructor;
 
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
-@Table(name = "shoppingList")
+@Table(name = "shopping_list")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ShoppingList {
-    private long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", nullable = false)
     private User owner;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "family_id", nullable = false)
     private Family family;
+
     private String name;
     private String description;
-    private ArrayList<Task> taskArrayList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "shoppingList", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Task> taskArrayList = new ArrayList<>();
+
     private Date createdAt;
     private Date updatedAt;
-    private String status;
+    private String status = StatusConfig.AVAILABLE.getStatus();
 }
