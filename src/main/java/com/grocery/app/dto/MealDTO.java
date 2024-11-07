08df282d182e8ee628d02c954a9e5382ac864630
littleamaterasu <clgtthangtung@gmail.com>
2024-11-07 09:13:21 +0700

    init MealDTO

--- src/main/java/com/grocery/app/dto/MealDTO.java
+++ src/main/java/com/grocery/app/dto/MealDTO.java
@@ -1,4 +1,26 @@
 package com.grocery.app.dto;
 
+import com.grocery.app.dto.family.FamilyDetailDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class MealDTO {
+    private Long mealId;
+    private UserDetailDTO userDetailDTO;
+    private FamilyDetailDTO familyDetailDTO;
+    private String term;
+    private String date;
+    private ArrayList<RecipeDTO> recipeDTOS;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+    private String status;
 }
