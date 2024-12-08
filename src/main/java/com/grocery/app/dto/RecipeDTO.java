194d15d4fca4925f9758733601ab023c07d45eff
littleamaterasu <clgtthangtung@gmail.com>
2024-12-08 16:40:55 +0700

    change id to user detail dto

--- src/main/java/com/grocery/app/dto/RecipeDTO.java
+++ src/main/java/com/grocery/app/dto/RecipeDTO.java
@@ -5,10 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class RecipeDTO {
     private Long id;
+    private UserDetailDTO userDetailDTO;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private ArrayList<FoodDTO> foodDTOs;
 }
