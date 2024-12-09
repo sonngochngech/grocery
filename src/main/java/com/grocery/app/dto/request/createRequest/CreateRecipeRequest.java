824d3d247ab78057ba2f36a46a729d94dc5866e8
littleamaterasu <clgtthangtung@gmail.com>
2024-12-09 08:25:04 +0700

    request interface recipe

--- src/main/java/com/grocery/app/dto/request/createRequest/CreateRecipeRequest.java
+++ src/main/java/com/grocery/app/dto/request/createRequest/CreateRecipeRequest.java
@@ -1,4 +1,16 @@
 package com.grocery.app.dto.request.createRequest;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CreateRecipeRequest {
+    private String name;
+    private Long userId;
 }
--- src/main/java/com/grocery/app/dto/request/updateRequest/UpdateRecipeRequest.java
+++ src/main/java/com/grocery/app/dto/request/updateRequest/UpdateRecipeRequest.java
@@ -1,4 +1,15 @@
 package com.grocery.app.dto.request.updateRequest;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UpdateRecipeRequest {
+    private Long id;
+    private String name;
 }
